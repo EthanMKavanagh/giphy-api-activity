2a73a0089ad2a0151359728609e335ed0418f53d
Mike Siddall <eatingartist@gmail.com>
2020-10-01 16:18:27 -0500

    committig for pull

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
-import axios from 'axios';
-import { put, takeEvery } from 'redux-saga/effects';
-import createSagaMiddleware from 'redux-saga';
-import logger from 'redux-logger';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+import axios from "axios";
+import { put, takeEvery } from "redux-saga/effects";
+import createSagaMiddleware from "redux-saga";
+import logger from "redux-logger";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 
 // CREATE REDUCERS HERE
 // --->
 
+function* fetchImageSaga(action) {
+  console.log("in fetchImageSaga", action);
+  let response = yield axios({
+    method: "GET",
+    url: "/api/category",
+  });
+  yield put({
+    type: "FETCH_IMAGE",
+    payload: response.data,
+  });
+}
+
 function* watcherSaga() {
-    // Input saga functions here
-    // ex. yield takeEvery('FETCH_GIF', fetchGifSaga);
+  // Input saga functions here
+  // ex. yield takeEvery('FETCH_GIF', fetchGifSaga);
+  yield takeEvery("FETCH_IMAGE", fetchImageSaga);
 }
 
 // CREATE SAGA MIDDLEWARE
@@ -21,13 +34,17 @@ const sagaMiddleware = createSagaMiddleware();
 
 // REDUX STORE
 const reduxStore = createStore(
-    combineReducers({
-        // ADD REDUCER HERE
-    }),
-    applyMiddleware(logger, sagaMiddleware)
-)
+  combineReducers({
+    // ADD REDUCER HERE
+  }),
+  applyMiddleware(logger, sagaMiddleware)
+);
 
 sagaMiddleware.run(watcherSaga);
 
-ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, 
-    document.getElementById('react-root'));
+ReactDOM.render(
+  <Provider store={reduxStore}>
+    <App />
+  </Provider>,
+  document.getElementById("react-root")
+);
