907c2ba8c46d9c0e75dff0a164624b6f64ed0041
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-10-01 13:33:02 -0500

    basic set up of index.js with logger, reducers, and saga

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import axios from 'axios';
+import { put, takeEvery } from 'redux-saga/effects';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+// CREATE REDUCERS HERE
+// --->
+
+function* watcherSaga() {
+    // Input saga functions here
+    // ex. yield takeEvery('FETCH_GIF', fetchGifSaga);
+}
+
+// CREATE SAGA MIDDLEWARE
+const sagaMiddleware = createSagaMiddleware();
+
+// REDUX STORE
+const reduxStore = createStore(
+    combineReducers({
+        // ADD REDUCER HERE
+    }),
+    applyMiddleware(logger, sagaMiddleware)
+)
+
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, 
+    document.getElementById('react-root'));
